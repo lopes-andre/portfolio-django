b70843af8af4be3b806a964b0f8e01e1be16053f
André Lopes <lopes.andre@outlook.com>
2021-09-23 18:26:40 -0300

    Update to teste querying from DB

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 def home(request):
-    return render(request, 'projects/home.html')
+    projects = Project.objects.all()
+    return render(request, 'projects/home.html', {
+        'projects': projects,
+    })
